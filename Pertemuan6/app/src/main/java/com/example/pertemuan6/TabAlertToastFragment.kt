a5957a127ad350e288fd96d09f87c400015c8e8d
Ibnu Shevayanto <ibnushevayanto@gmail.com>
2022-05-23 19:37:40 +0700

    add pertemuan 6

--- Pertemuan6/app/src/main/java/com/example/pertemuan6/TabAlertToastFragment.kt
+++ Pertemuan6/app/src/main/java/com/example/pertemuan6/TabAlertToastFragment.kt
@@ -1,17 +1,31 @@
 package com.example.pertemuan6
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 
 class TabAlertToastFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab_alert_toast, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_tab_alert_toast, container, false)
+
+        val btnShowAlert = view.findViewById<Button>(R.id.btnShowAlert)
+        btnShowAlert.setOnClickListener {
+            val alertBuilder = AlertDialog.Builder(view.context)
+            alertBuilder.setTitle("Android Alert")
+            alertBuilder.setPositiveButton("OKE") {
+                    _, _ -> Toast.makeText(view.context, "Show Toast", Toast.LENGTH_SHORT).show()
+            }
+            alertBuilder.show()
+        }
+        return view
     }
 }
--- Pertemuan6/app/src/main/java/com/example/pertemuan6/TabListViewFragment.kt
+++ Pertemuan6/app/src/main/java/com/example/pertemuan6/TabListViewFragment.kt
@@ -5,13 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.R.layout.simple_list_item_1
 
 class TabListViewFragment : Fragment() {
+    private lateinit var lvMobil: ListView
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab_list_view, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_tab_list_view, container, false)
+        lvMobil = view.findViewById(R.id.lv_mobil)
+
+        val mobil = listOf("Avanza", "Inova", "Lamborgini", "Grand Livina", "Xenia")
+        lvMobil.adapter = ArrayAdapter(view.context, simple_list_item_1, mobil)
+        return view
     }
 }
